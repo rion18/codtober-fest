d7d990328ca5ea43cede71ac61b4fa0a4072a029
Alvaro Lopez <rion18@hotmail.com>
2018-10-20 11:24:29 -0500

    Scaffold done.

---
+++ src/main/java/com/rion18/stackbuilders/codtoberfest/CodtoberfestApplication.java
@@ -0,0 +1,12 @@
+package com.rion18.stackbuilders.codtoberfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodtoberfestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodtoberfestApplication.class, args);
+	}
+}
---
+++ src/main/java/com/rion18/stackbuilders/codtoberfest/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.rion18.stackbuilders.codtoberfest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+}
